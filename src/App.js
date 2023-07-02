c9a34a6570a103f47d43f8d012d6f20a33f73940
usman <kohistani5525@gmail.com>
2023-07-02 18:14:26 +0500

    ''

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useReducer } from 'react'
 import './App.css'
-import Header from './component/Header'
-import CurrentBalance from './component/CurrentBalance'
-import Transactions from './component/Transactions'
-import AddTransaction from './component/AddTransaction'
+import Header from './component/Header.js'
+import CurrentBalance from './component/CurrentBalance.js'
+import Transactions from './component/Transactions.js'
+import AddTransaction from './component/AddTransaction.js'
 
 function App() {
   const [transactions, setTransactions] = useState([])
